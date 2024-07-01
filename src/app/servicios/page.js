2158df91fd5e7ef851e609807d8a3c43e23810a9
alejomayuri <alejomayuri@gmail.com>
2024-06-30 20:09:44 -0500

    correccion servicios en orden alfabetico

--- src/app/servicios/page.js
+++ src/app/servicios/page.js
@@ -15,27 +15,27 @@ export default function Nosotros() {
             <h2 className={katibeh.className}>Servicios</h2>
             <div>
               <ul>
-                <li><Link href="/servicios/para-ellas">PARA ELLAS</Link></li>
-                <li><Link href="/servicios/para-ellos">PARA EL</Link></li>
                 <li><Link href="/servicios/aparatologia-cosmetica">APARATOLOGÍA COSMÉTICA</Link></li>
-                <li><Link href="/servicios/laboratorio-de-patologia">LABORATORIO DE PATOLOGÍA</Link></li>
-              </ul>
-              <ul>
-                <li><Link href="/servicios/cardiologia">CARDIOLOGÍA</Link></li>
                 <li><Link href="/servicios/biopsias">BIOPSIAS</Link></li>
-                <li><Link href="/servicios/laboratorio-clinico">EXAMENES DE LAB CLÍNICO</Link></li>
-                <li><Link href="/servicios/mesoterapia-y-reflexologia">MESOTERAPIA Y REFLEXOLOGÍA</Link></li>
-              </ul>
-              <ul>
-                <li><Link href="#">TEST DE ALIENTO</Link></li>
-                <li><Link href="/servicios/imagenes">IMÁGENES</Link></li>
-                <li><Link href="/servicios/oncologia">ONCOLOGÍA</Link></li>
-                <li><Link href="/servicios/topico-y-farmacia">TÓPICO Y FARMACIA</Link></li>
-              </ul>
-              <ul>
-                <li><Link href="/servicios/inmunologia-y-alergia">INMULOGÍA Y ALERGIA</Link></li>
-                <li><Link href="/servicios/gastroenterologia">GASTROENTEROLOGÍA</Link></li>
+                <li><Link href="/servicios/cardiologia">CARDIOLOGÍA</Link></li>
                 <li><Link href="/servicios/chequeos-preventivos">CHEQUEOS PREVENTIVOS</Link></li>
+              </ul>
+              <ul>
+                <li><Link href="/servicios/laboratorio-clinico">EXAMENES DE LAB CLÍNICO</Link></li>
+                <li><Link href="/servicios/gastroenterologia">GASTROENTEROLOGÍA</Link></li>
+                <li><Link href="/servicios/imagenes">IMÁGENES</Link></li>
+                <li><Link href="/servicios/inmunologia-y-alergia">INMULOGÍA Y ALERGIA</Link></li>
+              </ul>
+              <ul>
+                <li><Link href="/servicios/laboratorio-de-patologia">LABORATORIO DE PATOLOGÍA</Link></li>
+                <li><Link href="/servicios/mesoterapia-y-reflexologia">MESOTERAPIA Y REFLEXOLOGÍA</Link></li>
+                <li><Link href="/servicios/oncologia">ONCOLOGÍA</Link></li>
+                <li><Link href="/servicios/para-ellos">PARA EL</Link></li>
+              </ul>
+              <ul>
+                <li><Link href="/servicios/para-ellas">PARA ELLAS</Link></li>
+                <li><Link href="#">TEST DE ALIENTO</Link></li>
+                <li><Link href="/servicios/topico-y-farmacia">TÓPICO Y FARMACIA</Link></li>
               </ul>
             </div>
         </div>
